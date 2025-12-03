fa42216b8e332ee6eb2978ef56d311092ee3aa6b
AndoGhevian <lenyvonbeer@gmail.com>
2025-12-03 11:53:07 +0400

    sandbox file inheriting newsandbox for new tests

---
+++ newsandbox/sandbox.asm
@@ -0,0 +1,14 @@
+section .data
+section .text
+GlobalLib:
+    mov rax,1
+    mov rdi,1
+    mov rsi,GlobalLibMessage
+    mov rdx,GlobalLibMessageLen
+    syscall
+
+    ret
+; this data goes inside text section adjacent to instruction bytes
+; it is safe becuase ret does not allow it to be reached.
+GlobalLibMessage db "Hello Global Lib!!!",10
+GlobalLibMessageLen equ $-GlobalLibMessage
